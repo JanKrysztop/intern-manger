40e6664d03b314c1a717ad93a54ee8f2b43f13d1
Jan Krysztop <jan.krysztop@dimark.local>
2024-10-15 15:03:36 +0200

    feat: add photo capturing

--- src/components/InternForm.vue
+++ src/components/InternForm.vue
@@ -1,78 +1,170 @@
 <script setup lang="ts">
+import { onBeforeUnmount, ref } from "vue";
 import { useDisplay } from "vuetify";
 import avatar from "@/assets/avatar.svg";
 
 const { smAndDown } = useDisplay();
+const firstName = ref<string | null>(null);
+const lastName = ref<string | null>(null);
+const valueRequired = [(v: any) => !!v || "Field is required"];
+const isCameraActive = ref<boolean>(false);
+const photo = ref<string | null>(null);
+const video = ref<HTMLVideoElement | null>(null);
+const canvas = ref<HTMLCanvasElement | null>(null);
+const stream = ref<MediaStream | null>(null);
+
+const startCamera = async (): Promise<void> => {
+  isCameraActive.value = true;
+  try {
+    stream.value = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "user" },
+    });
+    if (video.value) {
+      video.value.srcObject = stream.value;
+    }
+  } catch (error) {
+    console.error("Error accessing camera:", error);
+    isCameraActive.value = false;
+  }
+};
+
+const stopCamera = (): void => {
+  if (stream.value) {
+    stream.value.getTracks().forEach((track) => track.stop());
+    isCameraActive.value = false;
+  }
+};
+
+const capturePhoto = (): void => {
+  console.log("FLASH !");
+  const ctx = canvas.value?.getContext("2d");
+  if (ctx && video.value && canvas.value) {
+    canvas.value.width = video.value.videoWidth;
+    canvas.value.height = video.value.videoHeight;
+
+    ctx.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height);
+
+    photo.value = canvas.value.toDataURL("image/png");
+    console.log(photo.value);
+    stopCamera();
+  }
+};
+
+const internFormRef = ref();
+const handleFormSubmit = async () => {
+  const isFormValid = await internFormRef.value.validate();
+  if (isFormValid.valid) {
+    console.log();
+  }
+};
+
+onBeforeUnmount(() => {
+  stopCamera();
+});
 </script>
 
 <template>
   <v-container>
     <div class="text-h4 mb-6">Add Intern</div>
-    <v-row class="h-75">
-      <v-col cols="12" md="8" class="h-75 pb-10" :order="smAndDown ? 1 : 0">
-        <v-sheet class="d-flex flex-column pa-5 px-8 h-100 constraints">
-          <v-row class="mt-12 h-75">
-            <v-col cols="12" md="6">
-              <label for="first-name-input" class="font-weight-bold"
-                >First Name</label
-              >
-              <v-text-field
-                class="mt-2"
-                id="first-name-input"
-                placeholder="Enter intern first name"
-                density="compact"
-                variant="outlined"
-            /></v-col>
-            <v-col cols="12" md="6">
-              <label for="last-name-input" class="font-weight-bold"
-                >Last Name</label
-              >
-              <v-text-field
-                class="mt-2"
-                id="last-name-input"
-                placeholder="Enter intern last name"
-                density="compact"
-                variant="outlined"
-            /></v-col>
-          </v-row>
-
-          <v-btn
-            color="#459672"
-            flat
-            height="44"
-            width="120"
-            :block="smAndDown"
-            :class="smAndDown && 'mt-6'"
+    <v-form ref="internFormRef" @submit.prevent="handleFormSubmit">
+      <v-row class="h-75">
+        <v-col cols="12" md="8" class="h-75 pb-10" :order="smAndDown ? 1 : 0">
+          <v-sheet class="d-flex flex-column pa-5 px-8 constraints">
+            <v-row class="mt-12">
+              <v-col cols="12" md="6">
+                <label for="first_name_input" class="font-weight-bold"
+                  >First Name</label
+                >
+                <v-text-field
+                  v-model="firstName"
+                  :rules="valueRequired"
+                  id="first_name_input"
+                  class="mt-2"
+                  placeholder="Enter intern first name"
+                  density="compact"
+                  variant="outlined"
+              /></v-col>
+              <v-col cols="12" md="6">
+                <label for="last_name_input" class="font-weight-bold"
+                  >Last Name</label
+                >
+                <v-text-field
+                  v-model="lastName"
+                  :rules="valueRequired"
+                  id="last_name_input"
+                  class="mt-2"
+                  placeholder="Enter intern last name"
+                  density="compact"
+                  variant="outlined"
+              /></v-col>
+            </v-row>
+            <v-btn
+              id="inter_form_submit_btn"
+              type="submit"
+              color="#459672"
+              flat
+              height="44"
+              width="120"
+              :block="smAndDown"
+              :class="smAndDown && 'mt-6'"
+            >
+              <p class="text-body-1 mb-0">Add Intern</p>
+            </v-btn>
+          </v-sheet>
+        </v-col>
+        <v-col cols="12" md="4" class="h-75 pb-10">
+          <v-sheet
+            class="d-flex flex-column align-center pa-5 px-8 h-100 constraints"
+            rounded
           >
-            <p class="text-body-1 mb-0">Add Intern</p>
-          </v-btn>
-        </v-sheet>
-      </v-col>
-      <v-col cols="12" md="4" class="h-75 pb-10">
-        <v-sheet
-          class="d-flex flex-column align-center pa-5 px-8 h-100 constraints"
-          rounded
-        >
-          <v-row class="mt-10 h-75">
-            <v-avatar class="avatar" :image="avatar" size="140"></v-avatar>
-          </v-row>
-          <v-btn
-            class="w-100"
-            color="#636C73"
-            flat
-            height="44"
-            variant="outlined"
-          >
-            <v-icon class="mr-2">mdi-camera</v-icon>
-            <p class="text-body-1 mb-0">Add Photo</p>
-          </v-btn>
-        </v-sheet>
-      </v-col>
-    </v-row>
+            <v-avatar
+              v-if="!isCameraActive && !photo"
+              class="avatar mt-10"
+              :image="avatar"
+              size="140"
+            ></v-avatar>
+            <v-avatar
+              v-else-if="!isCameraActive && photo"
+              class="avatar mt-10"
+              :image="photo"
+              size="140"
+            ></v-avatar>
+            <video
+              v-else
+              ref="video"
+              autoplay
+              playsinline
+              width="140"
+              height="140"
+              class="video-circle avatar mt-10"
+            ></video>
+            <v-spacer></v-spacer>
+            <v-btn
+              @click="!isCameraActive ? startCamera() : capturePhoto()"
+              id="photo_btn"
+              class="w-100"
+              color="#636C73"
+              flat
+              height="44"
+              variant="outlined"
+            >
+              <v-icon class="mr-2">mdi-camera</v-icon>
+              <p v-if="!isCameraActive" class="text-body-1 mb-0">Add Photo</p>
+              <p v-else class="text-body-1 mb-0">Take Photo</p>
+            </v-btn>
+            <canvas ref="canvas" style="display: none"></canvas>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
 <style scoped>
+.video-circle {
+  border-radius: 50%;
+  object-fit: cover;
+}
 .avatar {
   border: 2px solid #f5f5f5;
 }
